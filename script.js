950fc77bd4663ca3a63d9643858a14531d0119d9
trohith89 <75958140+trohith89@users.noreply.github.com>
2023-07-02 17:04:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-document.getElementById("submit").addEventListener("click", ()=>{
-    const pi = 3.14;
-    let r = document.getElementById("radius").value;
-    let ans = (4/3)*pi*r*r*r;
-    document.getElementById("volume").value = ans;
-})
+function volume_sphere(){
+	document.getElementById("submit").addEventListener("click", ()=>{
+		const pi = 3.14;
+		let r = document.getElementById("radius").value;
+	    let ans = (4/3)*pi*r*r*r;
+	    document.getElementById("volume").value = ans;
+	});	
+}
+
+// document.getElementById("submit").addEventListener("click", ()=>{
+//     const pi = 3.14;
+//     let r = document.getElementById("radius").value;
+//     let ans = (4/3)*pi*r*r*r;
+//     document.getElementById("volume").value = ans;
+// })
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
