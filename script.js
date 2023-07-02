c05fc0797ab328da5d0ff1e2721169221fd6651c
trohith89 <75958140+trohith89@users.noreply.github.com>
2023-07-02 17:13:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function volume_sphere(){
 	let pi = 3.14;
 	let r = document.getElementById("radius").value;
 	let ans = (4/3)*pi*r*r*r;
-	document.getElementById("volume").value = ans;
+	document.getElementById("volume").inneHTML = ans;
 	
 }
 document.getElementById("submit").addEventListener("click", volume_sphere());
